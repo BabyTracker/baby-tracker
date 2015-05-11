caba9bb1cf0c5ac94921db74d498cf181ef10dba
Alex Lyman <alexjohnlyman@gmail.com>
2015-05-11 14:34:24 -0600

    Linked Kid-list and Create new Update through update button

--- frontend/app/kids/kids.js
+++ frontend/app/kids/kids.js
@@ -9,10 +9,17 @@ angular.module('myApp.kids', ['ngRoute'])
   });
 }])
 
-.controller('KidsCtrl', ['$scope','Restangular', function($scope, Restangular){
+.controller('KidsCtrl', ['$scope','Restangular', '$location', function($scope, Restangular, $location){
     Restangular.all('kids').getList()
         .then(function(kids){
           $scope.kids = kids;
 
         })
+
+    $scope.addUpdate = function() {
+        var confirmation = confirm("Let's create a new memory!");
+        if(confirmation) {
+                $location.path('/updates')
+        }
+    };
 }]);
