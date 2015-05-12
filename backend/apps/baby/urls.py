9ebb1d3530880fc344ea6e01cf810b168d9094f6
Alex Lyman <alexjohnlyman@gmail.com>
2015-05-12 13:32:37 -0600

    Added note setion to create new update view

--- backend/apps/baby/urls.py
+++ backend/apps/baby/urls.py
@@ -3,7 +3,7 @@ from views import KidList, UpdateList, SaveUpdate
 
 urlpatterns = patterns('',
     url('^kids/$', KidList.as_view(), name='family'),
-    url('^kids/(?P<pk>[0-9]+)/$', KidDetail.as_view(), name='kid-detail'),
+    # url('^kids/(?P<pk>[0-9]+)/$', KidDetail.as_view(), name='kid-detail'),
     url('^kids/(?P<pk>[0-9]+)/updates/$', UpdateList.as_view(), name='update'),
     url('^kids/(?P<pk>[0-9]+)/save-update/$', SaveUpdate.as_view(), name='save-update'),
     # url('^kids/(?P<pk>[0-9]+)/updates/(?P<pk>[0-9]+)/$', UpdateDetail.as_view(), name='update-detail'),
