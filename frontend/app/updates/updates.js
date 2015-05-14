e03b0232b8db595d2119f36d0c82fcf0265d2bf7
Alex Lyman <alexjohnlyman@gmail.com>
2015-05-14 09:30:10 -0600

    Worked on date display bug fixes

--- frontend/app/updates/updates.js
+++ frontend/app/updates/updates.js
@@ -21,7 +21,7 @@ angular.module('myApp.updates', ['ngRoute'])
             var day = 1000 * 60 * 60 * 24;
 
             var diffDays = Math.floor(diff/day);
-            console.log(diffDays);
+            //console.log(diffDays);
 
             var totalMonths = diffDays/30.25; // 16.1983
 
