07c49c7c5e9d001fb1e9501dcc4dd47a95741958
Felicia Wold <feliciawold@gmail.com>
2015-05-21 15:31:24 -0600

    rerouted url to fix data pull and correct bug

--- backend/apps/baby/urls.py
+++ backend/apps/baby/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     url('^kids/(?P<pk>[0-9]+)/$', KidView.as_view(), name='kid-view'),
     url('^new-kid/$', NewKid.as_view(), name='new-kid'),
     url('^kids/(?P<pk>[0-9]+)/updates/$', UpdateList.as_view(), name='update'),
-    url('^kids/(?P<pk>[0-9]+)/save-update/$', SaveUpdate.as_view(), name='save-update'),
+    url('^kids/save-update/$', SaveUpdate.as_view(), name='save-update'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     # url('^kids/(?P<pk>[0-9]+)/updates/(?P<pk>[0-9]+)/$', UpdateDetail.as_view(), name='update-detail'),
     )
